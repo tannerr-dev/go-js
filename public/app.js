24688c5eb195907a2ba7643570f14248110815fc
Tanner Reyhons <tannerr.dev@protonmail.com>
2025-04-06 13:09:39 -0500

    add search order and sort

--- public/app.js
+++ public/app.js
@@ -30,4 +30,16 @@ window.app = {
         const q = document.querySelector("input[type=search]").value;
         app.Router.go("/movies?q=" + q);
     },
+    searchOrderChange: (order) => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const q = urlParams.get("q");
+        const genre = urlParams.get("genre") ?? "";
+        app.Router.go(`/movies?q=${q}&order=${order}&genre=${genre}`);
+    },
+    searchFilterChange: (genre) => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const q = urlParams.get("q");
+        const order = urlParams.get("order") ?? "";
+        app.Router.go(`/movies?q=${q}&order=${order}&genre=${genre}`);
+    }
 }
