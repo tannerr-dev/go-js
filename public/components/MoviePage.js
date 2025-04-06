2fd8c934837deb57c1191b77edd9cf32743b7585
Tanner Reyhons <tannerr.dev@protonmail.com>
2025-04-06 09:10:31 -0500

    everything broke lol, now i have it working

--- public/components/MoviePage.js
+++ public/components/MoviePage.js
@@ -1,7 +1,7 @@
-export class MoviesPage extends HTMLElement {
+export class MoviePage extends HTMLElement {
     
 }
 // you dont really have to register componenet elements if you are only using the from 
 // the router??
 // but i guess we register it anyways
-customElements.define("movie-page", MoviesPage)
+customElements.define("movie-page", MoviePage)
